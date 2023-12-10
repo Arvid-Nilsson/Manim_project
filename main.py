d06c669a3a3b88f5ea4adebb0f3c564a47e25d5e
Arvid Nilsson <arvnil27@gmail.com>
2023-12-10 22:04:20 +0100

    adding arclength

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from manim import *
-import time
+import math
+from scipy.integrate import quad
 
 class differentParametrications(Scene):
     def construct(self):
@@ -102,7 +103,75 @@ class UnitCircle(Scene):
         t_label = Text("t = ").scale(0.8).to_corner(UL)
         t_value = DecimalNumber(0).scale(0.8).next_to(t_label, RIGHT)
 
+        equation1_lable = Text("Parametricering 1", font_size=22)
+        equation1_x = MathTex(r"x &= \cos t")
+        equation1_y = MathTex(r"x &= \sin t")
+        equation1 = VGroup(equation1_lable, equation1_x, equation1_y).arrange(DOWN, aligned_edge=LEFT)
+
+        equation2_lable = Text("Parametricering 2", font_size=22)
+        equation2_x = MathTex(r"x &= \cos 2t")
+        equation2_y = MathTex(r"x &= \sin 2t")
+        equation2 = VGroup(equation2_lable, equation2_x, equation2_y).arrange(DOWN, aligned_edge=LEFT)
+
         t_value.add_updater(lambda v: v.set_value(t.get_value()))
 
-        self.add(ax, curve, t_label, t_value, dot, dot2)
+        self.add(ax, curve, t_label, t_value, dot, dot2, VGroup(equation1, equation2).arrange(DOWN, aligned_edge=LEFT).to_corner(UR))
         self.play(t.animate.set_value(2 * PI), rate_func=linear, run_time=10)
+
+class arclength(Scene):
+    def func(self, t):
+        return np.array((np.cos(t), np.sin(t), 0))
+    
+    def integrand(self, x):
+        return math.sqrt((math.sin(x)**2) + ((-math.cos(x))**2))
+    
+    def construct(self):
+        ax = Axes(
+            x_range=[-2, 2, 1],
+            y_range=[-2, 2, 1],
+            x_length=6,
+            y_length=6,
+        ).add_coordinates()
+
+        t = ValueTracker(0)
+
+        curve = ax.plot_parametric_curve(self.func, t_range=[0, 2*np.pi], color=ORANGE)
+
+        dot = Dot(ax.c2p(1, 0))
+
+        def update_dot(dot):
+            t_value = t.get_value()
+            new_point = ax.input_to_graph_point(t_value, curve)
+            dot.move_to(new_point)
+
+        dot.add_updater(update_dot)
+        
+        text = Text("")
+        text1 = MathTex(r"\int")
+        text2 = MathTex(r"\int_{a}^{b}")
+        text3 = MathTex(r"\int_{a}^{b} \sqrt{\left(\frac{dy}{dt}\right)^2 \cdot \left(\frac{dx}{dt}\right)^2}")
+        text4 = MathTex(r"\int_{a}^{b} \sqrt{\left(\frac{dy}{dt}\right)^2 \cdot \left(\frac{dx}{dt}\right)^2} =").to_corner(UR).scale(0.5)
+        text6 = DecimalNumber(0).scale(0.5).next_to(text4, RIGHT)
+        text6.add_updater(lambda v: v.set_value(quad(self.integrand, 0, t.get_value())[0]))       
+        
+
+
+
+        self.add(text)
+        self.play(Transform(text, text1))
+        self.play(Transform(text, text2))
+        self.play(Transform(text, text3))
+        self.wait(3)
+        self.play(text.animate.to_corner(UR).scale(0.5))
+        self.play
+        self.play(FadeIn(ax))
+        self.wait(3)
+        self.add(curve)
+        self.wait(3)
+        self.play(Create(dot), Transform(text, text4), FadeIn(text6))
+        self.wait(3)
+        self.play(t.animate.set_value(2 * PI), rate_func=linear, run_time=10)
+        self.wait(3)
+
+
+       
