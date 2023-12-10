62abef2803f4abe3fcf2bdf097b6065666366a38
Arvid Nilsson <arvnil27@gmail.com>
2023-12-10 17:17:30 +0100

    Adding dots in unit circle animation

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class differentParametrications(Scene):
         self.wait(2)
         self.play(Transform(text_1 ,VGroup(text_5_1, text_5_2).arrange(DOWN, aligned_edge=LEFT)))
 
-class ExampleFunctionGraph(Scene):
+class parametricationsVisualized(Scene):
     def construct(self):
         ax = Axes(
             x_range=[-5, 5, 1],
@@ -67,5 +67,42 @@ class ExampleFunctionGraph(Scene):
         self.play(t.animate.set_value(2), rate_func = linear, run_time=10)
 
 
+class UnitCircle(Scene):
+    def func(self, t):
+        return np.array((np.cos(t), np.sin(t), 0))
 
+    def construct(self):
+        ax = Axes(
+            x_range=[-2, 2, 1],
+            y_range=[-2, 2, 1],
+            x_length=6,
+            y_length=6,
+        ).add_coordinates()
 
+        t = ValueTracker(0)
+
+        curve = ax.plot_parametric_curve(self.func, t_range=[0, 2*np.pi], color=RED)
+
+        dot = Dot(ax.c2p(1, 0))
+        dot2 = Dot(ax.c2p(1, 0))
+
+        def update_dot(dot):
+            t_val = t.get_value()
+            new_point = ax.input_to_graph_point(t_val, curve)
+            dot.move_to(new_point)
+
+        def update_dot2(dot):
+            t_val = t.get_value()
+            new_point = ax.input_to_graph_point(2*t_val, curve)
+            dot.move_to(new_point)
+
+        dot.add_updater(update_dot)
+        dot2.add_updater(update_dot2)
+
+        t_label = Text("t = ").scale(0.8).to_corner(UL)
+        t_value = DecimalNumber(0).scale(0.8).next_to(t_label, RIGHT)
+
+        t_value.add_updater(lambda v: v.set_value(t.get_value()))
+
+        self.add(ax, curve, t_label, t_value, dot, dot2)
+        self.play(t.animate.set_value(2 * PI), rate_func=linear, run_time=10)
